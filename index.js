e89e4dddb70847af82bbd8d2d6b73a3f546e33c7
Abdullah Sajjad <themuststudios@gmail.com>
2022-09-20 21:16:20 +0500

    Auto scroll to slider

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 
+var counter = 1 ;
+setInterval (function(){
+  document.getElementById ('radio'+ counter).click();
+  counter ++ ;
+  if (counter > 3) {
+    counter = 1 ;
+  }
+} , 2500 );
+
 
 
