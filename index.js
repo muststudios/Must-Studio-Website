8fb0c04730ce89734942103b0cdab1b84454e19f
Abdullah Sajjad <themuststudios@gmail.com>
2022-09-21 21:23:53 +0500

    Add chat box/contact us box

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-
-var counter = 1 ;
-setInterval (function(){
-  document.getElementById ('radio'+ counter).click();
-  counter ++ ;
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).click();
+  counter++;
   if (counter > 3) {
-    counter = 1 ;
+    counter = 1;
   }
-} , 2500 );
+}, 2500);
 
+document.getElementById("mymodal").addEventListener("click", ()=>{
+  document.getElementById("hide_btn_js").click();
+});// To close the contact us form by clicking anywhere
 
 
